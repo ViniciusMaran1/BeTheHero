1efca384bea694e7bccf29b85eb081e1c585449c
ViniciusMaran1 <42592895+ViniciusMaran1@users.noreply.github.com>
2020-03-29 14:02:06 -0300

    Mobile da Aplicação

---
+++ mobile/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Routes from './src/routes';
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+import 'expo-mail-composer';
+
+export default function App() {
+  return (
+    <Routes/>    
+  );
+}
